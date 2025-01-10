29824b6ab368c3d598876de6fd3008139e60367a
mahdi-saadat <mahdi.python.au@gmail.com>
2025-01-10 11:23:35 +1000

    remove java
    remove java

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('additionForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    const a = parseFloat(document.getElementById('inputA').value);
-    const b = parseFloat(document.getElementById('inputB').value);
-
-    const c = a + b;
-    document.getElementById('result').textContent = `The result of a + b is: ${c}`;
-});
